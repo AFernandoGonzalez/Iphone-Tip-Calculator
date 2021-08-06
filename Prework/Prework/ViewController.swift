7a7da1be9218574b3383bbc4d59f60ff59009e66
Fernando Gonzalez <58346389+AFernandoGonzalez@users.noreply.github.com>
2021-08-05 20:12:05 -0400

    Add files via upload

--- Prework/Prework/ViewController.swift
+++ Prework/Prework/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var customTipPercentage: UITextField!
+    @IBOutlet weak var totalCustomtip: UILabel!
+    @IBOutlet weak var btnTipCalculate: UIButton!
     
     
     override func viewDidLoad() {
@@ -26,7 +29,7 @@ class ViewController: UIViewController {
         let bill = Double(billAmountTextField.text!) ?? 0
         
         //Get Total tip by multiplying tip * tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
+        let tipPercentages = [0.15, 0.18, 0.2, 0.3]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
@@ -36,5 +39,34 @@ class ViewController: UIViewController {
         totalLabel.text = String(format: "$%.2f", total)
         
     }
+    
+    
+    @IBAction func calculateTipAmount(_ sender: Any) {
+        
+        let tipAmount:Double = (Double(totalCustomtip.text!)! / 100) * Double(billAmountTextField.text!)!
+        totalCustomtip.text = "\(tipAmount)"
+    }
+    
+    
+    
+    
+    @IBAction func calculateCustomTip(_ sender: UITextField) {
+        
+        let billTotal = NSString(string: billAmountTextField.text ?? "No value").doubleValue
+         
+        // 2
+        let tip = NSString(string: sender.text ?? "No value").doubleValue
+         
+        // 3
+        let convertedTip = (tip / 100)
+         
+        // 4
+        let tipTotal = billTotal * convertedTip
+         
+        // 5
+        totalCustomtip.text = "$\(String(billTotal + tipTotal))"
+    }
+    
+    
 }
 
